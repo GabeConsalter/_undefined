5a1833cab48ff0c436a310bf3f9a8d657c7fc3cc
GabeConsalter <consaltergabriel@gmail.com>
2018-08-24 15:44:26 -0300

    Config Firebase

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD6Q0MNDkiBTxBxL4WvHawO0i9Gkw4kHkY",
+  authDomain: "undefined-cfda5.firebaseapp.com",
+  databaseURL: "https://undefined-cfda5.firebaseio.com",
+  storageBucket: "undefined-cfda5.appspot.com"
+};
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    
+    Firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <View style={styles.container}>
